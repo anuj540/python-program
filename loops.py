7bb00d49addbcb46d92c5559cfb70ae85c5d30fb
anuj <anuj@gmail.com>
2024-11-28 00:10:00 +0530

    first commit

---
+++ loops.py
@@ -0,0 +1,66 @@
+# count=1
+# while count<=5:
+
+#     print("hello")
+#     count=count+1
+# print(count)    
+
+#print number 1 to 5
+# a=1
+# while a<=5:
+#     print(a)
+#     a+=1
+# print("loop end")    
+
+
+#print reverse number 5 to 1
+
+# a=5
+# while a>=1:
+#     print(a)
+#     a-=1
+
+# a=1
+# while a<=100:
+#     print(a)
+#     a+=1
+# print("loops end")    
+
+# a=100
+# while a>=1:
+#     print(a)
+#     a-=1
+
+#tables
+# n= int(input("enter a number"))
+# i=1
+# while i<=10:
+#     print(n*i)
+#     i+=
+
+
+#break key word
+
+# a = 1
+# while a <= 10:
+#     print(a)
+#     if (a == 6):
+#         break
+    
+#     a+=1
+# print("loop end")    
+
+# continue key word
+
+# a = 1
+# while a<=5:
+#     if(a==4):
+#         a+=1
+#         continue
+#     print(a)
+#     a+=1
+
+
+
+#for loops
+
