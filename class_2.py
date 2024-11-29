97b327f4945e5f0c6f0898dbc9872fd99602a60a
anuj <anuj@gmail.com>
2024-11-30 00:26:24 +0530

    str

---
+++ class_2.py
@@ -0,0 +1,55 @@
+# str1="this is a string.\nwe are creating it in python"
+# print(str1)
+
+#concatenation
+# a="Anuj"
+# b="Rohilla"
+# print(a+" "+b)
+
+#length of str
+# a= "anuj"
+# print(len(a))
+
+
+#indexing
+
+# an = "anuj"
+# a=an[2]
+# print(a)
+
+#slicing
+
+# a="apple mango"
+# print(a[:3])
+
+#negative slicing index
+
+# a="applee"
+# print(a[-4:-1])
+
+#String function
+#1. str.endswith()
+# a=" I am study in Pg tech"
+# print(a.endswith("ch0"))
+
+#2.str.captalize()
+# a="i am study in Pg tech"
+# print(a.capitalize())
+
+#3. replace function
+
+# a=" I am study in Pg tech"
+# print(a.replace("i", "me"))
+
+# 4 find function
+
+# a="I am study in Pg tech"
+# print(a.find("study"))
+
+#5. count function
+# a=" I am study in Pg tech"
+# print(a.count("e"))
+
+# name= input("enter your first name")
+# print("length your name is", len(name))
+
