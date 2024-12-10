566818da4c04fabaa3eb8392c5134a16471e291d
anuj <anuj@gmail.com>
2024-12-10 13:41:08 +0530

    list

---
+++ list_loop.py
@@ -0,0 +1,20 @@
+# marks=[10.12,13,14]
+# print(marks)
+# print(type(marks))
+
+# student=["Anuj",12.2, "Panipat"]
+# # print(student[0])
+# student[0]= "Mohit"
+# print(student)
+
+# marks=[10,12,10,11]
+# print(marks[-3:-1])
+
+# list=[1,2,3,4]
+# list.append(10)
+# print(list)
+
+# list=[10,7,5,1]
+# print(list.append(2))
+# print(list.sort())
+# print(list)
